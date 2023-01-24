aa273b298c6c42848285060015e8d01dc7382e57
Henthony <henthony555@gmail.com>
2023-01-24 17:14:42 -0300

    Fim

--- database/config.js
+++ database/config.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
+  //port: '',
   password: '',
   database: 'mylist',
   waitForConnections: true,
